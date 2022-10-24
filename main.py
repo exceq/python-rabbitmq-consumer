8a8e0009989ee9757b1ae5d55ee64c328328a693
exceq <mihail.petuxov@yandex.ru>
2022-10-24 13:39:59 +0500

    Добавлен redis в качестве кэша

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
 import json
-from enum import auto
+from typing import Optional
 
 import pika
+import redis as redis
 import requests
 import sys
 from os import getenv
 
+cache = redis.Redis(host=getenv("CACHE_HOST"))
+app_hostname = getenv("APP_HOSTNAME")
+
+
+def fetch_status_from_internet(url: str) -> int:
+    get = requests.get(url)
+    return get.status_code
+
+
+def get_from_cache(cache_key: str) -> Optional[int]:
+    value = cache.get(cache_key)
+    return value.decode('utf-8') if value else None
+
+
+def set_cache(cache_key: str, status_code: int) -> None:
+    cache.set(cache_key, status_code, ex=60)
+
+
+def get_status(url: str) -> int:
+    cache_key = f"url-{url}"
+    status_code = get_from_cache(cache_key)
+    print("Status code of cached url:", url, status_code)
+    if status_code is None:
+        status_code = fetch_status_from_internet(url)
+        set_cache(cache_key, status_code)
+
+    return status_code
+
 
 def handle_message(ch, method, properties, body):
     link_json = json.loads(body)
-    get = requests.get(link_json["url"])
-    url = f'http://nginx:80/link/{link_json["id"]}'
-    request_body = {"status": get.status_code}
+    url = f'http://{app_hostname}/link/{link_json["id"]}'
+    request_body = {"status": get_status(link_json["url"])}
     requests_put = requests.put(url, json=request_body, timeout=10)
     requests_put.raise_for_status()
 
